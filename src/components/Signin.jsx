298e23f093855bf638d58d8c6260c720f2afcc5d
eugenemuthenya-debug <eugenemuthenya@gmail.com>
2026-05-18 19:40:12 +0300

    move to  api.js to src

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Navbar from './Navbar'
-import API from '../../api'
+import API from '../api'
 
 const Signin = () => {
 
